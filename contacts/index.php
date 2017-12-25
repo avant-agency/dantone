9cdf6cecb2a8f3a3ca43c7857f3dde555a724487
galinakost <kostgalina@gmail.com>
2017-12-25 13:25:57 +0200

    Добавлен отступ между соцсетями

--- contacts/index.php
+++ contacts/index.php
@@ -91,6 +91,7 @@ $APPLICATION->SetTitle("Контакты | Dantone");
 <br />
 <div align="center">
 <div class="fb-page" data-href="https://www.facebook.com/dantonehome/" data-tabs="timeline" data-width="270" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/dantonehome/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/dantonehome/">Dantone HOME</a></blockquote></div></div>
+			<br />
 <div align="center">
 <iframe src="//widget.instagramm.ru/?imageW=4&imageH=2&thumbnail_size=56&type=0&typetext=dantonehome&head_show=1&profile_show=1&shadow_show=0&bg=255,255,255,1&opacity=true&head_bg=46729b&subscribe_bg=ad4141&border_color=c3c3c3&head_title=" allowtransparency="true" frameborder="0" scrolling="no" style="border:none;overflow:hidden;width:270px;height:273px;"></iframe>		
 </div>
