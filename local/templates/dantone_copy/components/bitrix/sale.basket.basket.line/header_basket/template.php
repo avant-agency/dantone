ff3e92a6fed30a3ed9f34466f96cf2641b559262
galinakost <kostgalina@gmail.com>
2018-01-17 04:21:02 +0200

    динамическая область в верхней корзине

--- local/templates/dantone_copy/components/bitrix/sale.basket.basket.line/header_basket/template.php
+++ local/templates/dantone_copy/components/bitrix/sale.basket.basket.line/header_basket/template.php
@@ -1,4 +1,6 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+$this->setFrameMode(true);
+
 $cartId = "bx_basket".$this->randString();
 $arParams['cartId'] = $cartId;
 
@@ -11,7 +13,7 @@ var <?=$cartId?> = new BitrixSmallCart;
 <?/*<div id="<?=$cartId?>" class="<?=$cartStyle?>"><?*/
 	/** @var \Bitrix\Main\Page\FrameBuffered $frame */
 		//$frame = $this->createFrame($cartId, false)->begin();
-		require(realpath(dirname(__FILE__)).'/ajax_template.php');
+require(realpath(dirname(__FILE__)).'/ajax_template.php');
 		//$frame->beginStub();
 		//$arResult['COMPOSITE_STUB'] = 'Y';
 		//require(realpath(dirname(__FILE__)).'/top_template.php');
